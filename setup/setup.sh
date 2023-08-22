d1e9feffdd871010036ff9a9986395aee19516e3
erupturatis <barbulescu.eugeno@gmail.com>
2023-08-22 20:23:34 +0300

    Updating submodule on marți 22 august 2023, 20:23:34 +0300

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # installs
-
+chmod +x installs_setup.sh
+source installs_setup.sh
 # utilities
-
+chmod +x utilities_setup.sh
+source utilities_setup.sh
 # ssh key
+chmod +x ssh_keys_setup.sh
+source ssh_keys_setup.sh
+
+echo("now you only need to pull the configs and you are done")
+
 
 
--- setup/startup.sh
+++ setup/startup.sh
@@ -1,7 +1,5 @@
-
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 # set up caps lock as hero key
 xmodmap -e "clear lock"
 xmodmap -e "keycode 66 = Super_L"
-
-# pull dotfiles and commit them to github
 
